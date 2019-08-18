d2314213156402248b783914c4a619fb59986abd
igorb22 <igorb22@live.com>
2019-08-17 22:51:00 -0300

    corrigindo inserts no ambiente operacional

--- Ambiente OLTP/Scripts/AMBIENTE_OPERACIONAL_PROJETO_QUERO_ACOMPANHANTE.sql
+++ Ambiente OLTP/Scripts/AMBIENTE_OPERACIONAL_PROJETO_QUERO_ACOMPANHANTE.sql
@@ -48,29 +48,6 @@ CREATE TABLE TipoAcompanhamento(
 	data_atualizacao DATETIME NOT NULL,
 	PRIMARY KEY(idTipoAcompanhamento)
 )
-
-CREATE TABLE Servico (
-   idServico INT IDENTITY(1,1) NOT NULL,
-   idCliente INT NOT NULL,
-   idAcompanhante INT NOT NULL,	
-   status VARCHAR(45) NOT NULL CHECK(status IN ('EM ANDAMENTO', 'CANCELADA', 'FINALIZADA')),
-   data_atualizacao DATETIME NOT NULL,
-   PRIMARY KEY(idServico),
-   FOREIGN KEY (idTipoAcompanhamento) REFERENCES TipoAcompanhamento(idTipoAcompanhamento)
-)
-
-CREATE TABLE Mensagem (
-   idMensagem INT IDENTITY(1,1) NOT NULL,
-   mensagem VARCHAR(300) NULL DEFAULT NULL,
-   dataEHora DATETIME NULL DEFAULT NULL,
-   idRemetente INT NOT NULL,
-   idDestinatario INT NOT NULL,
-   idServico INT NOT NULL,
-   data_atualizacao DATETIME NOT NULL,
-   PRIMARY KEY(idMensagem),
-   FOREIGN KEY (idServico) REFERENCES Servico(idServico)
-)
-
 CREATE TABLE Oportunidade (
    idOportunidade INT IDENTITY(1,1) NOT NULL,
    data_solicitacao DATETIME NOT NULL,
@@ -86,6 +63,30 @@ CREATE TABLE Oportunidade (
    PRIMARY KEY(idOportunidade),
    FOREIGN KEY (idTipoAcompanhamento) REFERENCES TipoAcompanhamento(idTipoAcompanhamento),
    FOREIGN KEY (idCliente) REFERENCES Usuario(idUsuario),
+)
+
+CREATE TABLE Servico (
+   idServico INT IDENTITY(1,1) NOT NULL,
+   idOportunidade INT NOT NULL,
+   idCliente INT NOT NULL,
+   idAcompanhante INT NOT NULL,	
+   status VARCHAR(45) NOT NULL CHECK(status IN ('EM ANDAMENTO', 'CANCELADA', 'FINALIZADA')),
+   data_atualizacao DATETIME NOT NULL,
+   PRIMARY KEY(idServico),
+   FOREIGN KEY (idOportunidade) REFERENCES Oportunidade(idOportunidade)
+
+)
+
+CREATE TABLE Mensagem (
+   idMensagem INT IDENTITY(1,1) NOT NULL,
+   mensagem VARCHAR(300) NULL DEFAULT NULL,
+   dataEHora DATETIME NULL DEFAULT NULL,
+   idRemetente INT NOT NULL,
+   idDestinatario INT NOT NULL,
+   idServico INT NOT NULL,
+   data_atualizacao DATETIME NOT NULL,
+   PRIMARY KEY(idMensagem),
+   FOREIGN KEY (idServico) REFERENCES Servico(idServico)
 )
 
 CREATE TABLE DetalhesEncontro(
@@ -147,6 +148,8 @@ DROP PROCEDURE SP_INSERE_USUARIOS
 DROP PROCEDURE SP_INSERE_SOLICITACOES_SERVICO
 DROP PROCEDURE SP_INSERIR_NEGOCIACAO
 
+GO
+
 -- INSERINDO CLIENTES E ACOMPANHANTES  
 CREATE PROCEDURE SP_INSERE_USUARIOS 
 AS
@@ -174,10 +177,7 @@ AS
 			  (5,120.00,'00012A-0',GETDATE())
 	END
 
-
-
 GO
-
 
 -- INSERINDO OPORTUNIDADES, CANDIDATURAS, SOLICITACOES DE SERVICO E TIPOS DE SERVICO
 CREATE PROCEDURE SP_INSERE_SOLICITACOES_SERVICO
@@ -193,31 +193,24 @@ AS
 			  ('ACOMPANHAMENTO PARA PESSOAS COM NECESSIDADES ESPECIAIS','ESTE ACOMPANHANTE OFERECE SERVIÇOS DE ACOMPANHAMENTO PARA PESSOAS COM NECESSIDADES ESPECIAIS TAIS COMO DEFICIÊNCIA FÍSICA',GETDATE())
 
 
-		INSERT INTO Oportunidade (descricao,titulo,idCliente,idTipoAcompanhamento,idServico,status,data_atualizacao)
-		VALUES('PROCURO UMA PESSOA PARA FAZER UM ACOMPANHAMENTO MÉDICO COMIGO NO PRÓXIMO FINAL DE SEMANA 21-22/07, HORARIOS A COMBINAR',  'VAGA PARA ACOMPANHANTE MEDICO',6,1,NULL,'ABERTA',GETDATE()),
-			  ('PROCURO UMA PESSOA PARA FAZER ACOMPANHAMENTO INFANTIL PARA OS MEUS FILHOS NO PROXIMO FINAL DE SEMANA 21-22/07, HORARIOS A COMBINAR',  'VAGA PARA BABA',6,2,NULL,'ABERTA',GETDATE()),
-			  ('PROCURO UMA PESSOA PARA FAZER UM ACOMPANHAMENTO ESCOLAR COMIGO', 'VAGA PARA PROFESSORA DE BANCA',1,5,NULL,'ABERTA',GETDATE())
+		INSERT INTO Oportunidade (data_solicitacao,titulo,descricao,idTipoAcompanhamento,idCliente,idAcompanhante,idAcompanhnatePreferido,status,EhPublica,data_atualizacao)
+		VALUES(GETDATE(), 'VAGA PARA ACOMPANHANTE MEDICO','PROCURO UMA PESSOA PARA FAZER UM ACOMPANHAMENTO MÉDICO COMIGO NO PRÓXIMO FINAL DE SEMANA 21-22/07, HORARIOS A COMBINAR',1,6,NULL,5,'ABERTA',0,GETDATE()),
+			  (GETDATE(), 'VAGA PARA BABA','PROCURO UMA PESSOA PARA FAZER ACOMPANHAMENTO INFANTIL PARA OS MEUS FILHOS NO PROXIMO FINAL DE SEMANA 21-22/07, HORARIOS A COMBINAR',2,6,NULL,NULL,'ABERTA',1,GETDATE()),
+			  (GETDATE(), 'VAGA PARA PROFESSORA DE BANCA','PROCURO UMA PESSOA PARA FAZER UM ACOMPANHAMENTO ESCOLAR COMIGO',5,1,NULL,NULL,'ABERTA',1,GETDATE())
 		
-
 		INSERT INTO Candidatura (idAcompanhante,idOportunidade,data_atualizacao)
-		VALUES(5,1,GETDATE()),
-			  (4,2,GETDATE()),
+		VALUES(4,2,GETDATE()),
 			  (3,2,GETDATE()),
 			  (3,3,GETDATE())
+		
+		UPDATE Oportunidade SET idAcompanhante = 5, status = 'OCUPADA',data_atualizacao = GETDATE() where idOportunidade = 1
+		UPDATE Oportunidade SET idAcompanhante = 4, status = 'OCUPADA',data_atualizacao = GETDATE() where idOportunidade = 2
+		UPDATE Oportunidade SET idAcompanhante = 3, status = 'OCUPADA',data_atualizacao = GETDATE() where idOportunidade = 3
 
-		INSERT INTO Servico (idCliente,idAcompanhante,idTipoAcompanhamento,dataSolicitacao,status,data_atualizacao)
-		VALUES(1,4,3,GETDATE(),'PENDENTE',GETDATE()),
-			  (2,3,5,GETDATE(),'PENDENTE',GETDATE()),
-			  (1,3,5,GETDATE(),'PENDENTE',GETDATE()),
-			  (6,4,3,GETDATE(),'PENDENTE',GETDATE())
-
-		UPDATE Oportunidade SET idServico = 3,data_atualizacao = GETDATE() WHERE idOportunidade = 3
-
-		UPDATE Servico SET status = 'ACEITA',data_atualizacao = GETDATE() WHERE idServico = 1;
-		UPDATE Servico SET status = 'RECUSADA',data_atualizacao = GETDATE() WHERE idServico = 2;
-		UPDATE Servico SET status = 'ACEITA',data_atualizacao = GETDATE() WHERE idServico = 3;
-		UPDATE Servico SET status = 'ACEITA',data_atualizacao = GETDATE() WHERE idServico = 4;
-
+		INSERT INTO Servico (idOportunidade,idCliente,idAcompanhante,status,data_atualizacao)
+		VALUES(1,6,5,'EM ANDAMENTO',GETDATE()),
+			  (2,6,4,'EM ANDAMENTO',GETDATE()),
+			  (3,1,3,'EM ANDAMENTO',GETDATE())
 	END
 
 GO
@@ -226,63 +219,72 @@ GO
 CREATE PROCEDURE SP_INSERIR_NEGOCIACAO
 AS 
 	BEGIN
-
+	
 		INSERT INTO Mensagem (mensagem,dataEHora,idRemetente,idDestinatario,idServico,data_atualizacao)
-		VALUES ('Olá bom dia, preciso de uma pessoa para realizar algumas tarefas domésticas na minha casa',GETDATE(),1,4,1,GETDATE()),
-			   ('qual o dia ?',GETDATE(),4,1,1,GETDATE()),
-			   ('dia 25/07, você pode ?',GETDATE(),1,4,1,GETDATE()),
-			   ('posso sim, o dia todo ?',GETDATE(),4,1,1,GETDATE()),
-			   ('não, só das 8 até as 12 ',GETDATE(),1,4,1,GETDATE()),
-			   ('pronto, pode confirmar o encontro',GETDATE(),4,1,1,GETDATE()),
-			   ('Certo.',GETDATE(),1,4,1,GETDATE())
+		VALUES ('Olá bom dia, preciso de uma pessoa para realizar um acompanhamento doméstico com a minha avó',GETDATE(),6,5,1,GETDATE()),
+			   ('qual o dia ?',GETDATE(),5,6,1,GETDATE()),
+			   ('dia 25/07, você pode ?',GETDATE(),6,5,1,GETDATE()),
+			   ('posso sim, o dia todo ?',GETDATE(),5,6,1,GETDATE()),
+			   ('não, só das 8 até as 12 ',GETDATE(),6,5,1,GETDATE()),
+			   ('pronto, pode confirmar o encontro',GETDATE(),5,6,1,GETDATE()),
+			   ('Certo.',GETDATE(),6,5,1,GETDATE())
 
 		-- Detalhes Primeiro Encontro
 		INSERT INTO DetalhesEncontro (idServico,dataServico,horaInicio,horaFim,estado,cidade,bairro,rua,nLocal,referencia,valor,data_atualizacao)
-		VALUES(1,'20190725','08:00:00','12:00:00','SE','ITABAIANA','CENTRO','AV. BOANERGES PINHEIRO',144,'',200.00,GETDATE())
+		VALUES(1,'20190725','08:00:00','12:00:00','SE','ITABAIANA','CENTRO','AV. BOANERGES PINHEIRO',144,'',580.00,GETDATE())
 
 
 		/*-----------------------------------------------------------------------------------------------------------------------------------*/	
 		
 		INSERT INTO Mensagem (mensagem,dataEHora,idRemetente,idDestinatario,idServico,data_atualizacao)
-		VALUES('Oi',GETDATE(),1,4,3,GETDATE()),
-		      ('Olá',GETDATE(),4,1,3,GETDATE()),
-			  ('Gostaria de marcar um horario para reforço escolar na disciplina de fisica',GETDATE(),1,4,3,GETDATE()),
-			  ('Estou disponivel de segunda a sexta das 8 as 17 horas, tenho local próprio',GETDATE(),4,1,3,GETDATE()),
-			  ('Quinta feira dia 25/07 das 14 as 16 você pode me atender ? onde você atende ? ',GETDATE(),1,4,3,GETDATE()),
-			  ('Posso sim. É no endereço cadastrado no meu perfil, dê uma olhadinha aí... ',GETDATE(),4,1,3,GETDATE()),
-			  ('Pronto, to agendando aqui pelo aplicativo.',GETDATE(),1,4,3,GETDATE()),
-			  ('Ok!. Até lá',GETDATE(),4,1,3,GETDATE())
+		VALUES('Oi',GETDATE(),6,4,2,GETDATE()),
+		      ('Olá',GETDATE(),4,6,2,GETDATE()),
+			  ('Gostaria de marcar um horario pra cuidar do meu filho no final de semana, voce tem disponibilidade?',GETDATE(),6,4,2,GETDATE()),
+			  ('tenho sim, quais são os horários ?',GETDATE(),4,6,2,GETDATE()),
+			  ('sabado, das 09 as 15 horas ',GETDATE(),6,4,2,GETDATE()),
+			  ('pronto, pode agendar o acompanhamento ',GETDATE(),4,6,2,GETDATE()),
+			  ('certo, agendado!.',GETDATE(),6,4,2,GETDATE())
 		
-		UPDATE Oportunidade SET status = 'OCUPADA',data_atualizacao = GETDATE() WHERE idOportunidade = 3
-
 		-- DETALHES SEGUNDO ENCONTRO
 		INSERT INTO DetalhesEncontro (idServico,dataServico,horaInicio,horaFim,estado,cidade,bairro,rua,nLocal,referencia,valor,data_atualizacao)
-		VALUES(3,'20190725','08:00:00','12:00:00','SE','ITABAIANA','CENTRO','RUA CAPITAO JOSÉ FERREIRA',232,'',80.00,GETDATE())
+		VALUES(2,'20190725','09:00:00','15:00:00','SE','ITABAIANA','CENTRO','RUA CAPITAO JOSÉ FERREIRA',232,'',300.00,GETDATE())
 		
 		
 		/*-----------------------------------------------------------------------------------------------------------------------------------*/
 
 
 		INSERT INTO Mensagem (mensagem,dataEHora,idRemetente,idDestinatario,idServico,data_atualizacao)
-		VALUES ('Você trabalha como babá ?',GETDATE(),6,4,4,GETDATE()),
-			   ('Trabalho sim moço',GETDATE(),4,6,4,GETDATE()),
-			   ('tenho dois filhos pequenos, um de 5 anos e outro de 2',GETDATE(),6,4,4,GETDATE()),
-			   ('Você poderia cuidar deles na quinta feira pela manhã ? ',GETDATE(),6,4,4,GETDATE()),	
-			   ('quinta feira dia 25/07 ? ',GETDATE(),4,6,4,GETDATE()),
-			   ('sim!',GETDATE(),6,4,4,GETDATE()),
-			   ('infelizmente já tenho compromisso marcado, não pode ser outro dia ?',GETDATE(),4,6,4,GETDATE()),
-			   ('não pode não, vou cancelar a solicitação',GETDATE(),6,4,4,GETDATE()),
-			   ('certo!.',GETDATE(),4,6,4,GETDATE())
+		VALUES ('olá, voce poderia me ajudar com reforço esoclar en fisica ?',GETDATE(),1,4,3,GETDATE()),
+			   ('posso sim, moço',GETDATE(),4,1,3,GETDATE()),
+			   ('quinta feira a tarde dia 22/08 voce tem disponibilidade',GETDATE(),1,4,3,GETDATE()),
+			   ('tenho sim',GETDATE(),4,1,3,GETDATE()),	
+			   ('marcado então, vou agendar aqui',GETDATE(),1,4,3,GETDATE()),
+			   ('certo, até lá',GETDATE(),4,1,3,GETDATE())
+		
 
-		-- ENCONTRO CANCELADO
-		UPDATE Servico SET status = 'CANCELADA',data_atualizacao = GETDATE() WHERE idServico = 4;
+		INSERT INTO DetalhesEncontro (idServico,dataServico,horaInicio,horaFim,estado,cidade,bairro,rua,nLocal,referencia,valor,data_atualizacao)
+		VALUES(3,'20190822','14:00:00','18:00:00','SE','ITABAIANA','CENTRO','PERCILIO ANDRADE',507,'',80.00,GETDATE())
+		
 
 		/*-----------------------------------------------------------------------------------------------------------------------------------*/
 
-		INSERT INTO Transacao (idServico,dataEHora,data_atualizacao)
-		VALUES(1,'20190725',GETDATE()),
-			  (3,'20190725',GETDATE())
+		/*Serviço cancelado*/
+		INSERT INTO Mensagem (mensagem,dataEHora,idRemetente,idDestinatario,idServico,data_atualizacao)
+		VALUES('Olá, tenho uma má noticia, terei que cancelar o acompanhamento por motivo de frça maior',GETDATE(),4,6,2,GETDATE()),
+			  ('Sério isso ?',GETDATE(),6,4,2,GETDATE()),
+			  ('Sim, Infelizmente não vai dar',GETDATE(),4,6,2,GETDATE()),
+			  ('tudo bem então',GETDATE(),6,4,2,GETDATE())
+			  
+		UPDATE Servico SET status = 'CANCELADA', data_atualizacao = GETDATE() WHERE idServico = 2;
 
+		
+		/*-----------------------------------------------------------------------------------------------------------------------------------*/
+		INSERT INTO Transacao (idServico,dataEHora,pagamento_avista,data_atualizacao)
+		VALUES(1,'20190725',0,GETDATE()),
+			  (3,'20190822',1,GETDATE())
+
+
+		/*finalizando encontros*/
 		UPDATE Servico SET status = 'FINALIZADA', data_atualizacao = GETDATE() WHERE idServico = 1;
 		UPDATE Servico SET status = 'FINALIZADA', data_atualizacao = GETDATE() WHERE idServico = 3;
 	
